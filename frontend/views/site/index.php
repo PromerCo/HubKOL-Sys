20c45faa1b1f48c989ee06ddfa0ec790d28f2e4a
Clown <rd@promer.co>
2019-09-02 21:51:58 +0800

    modify

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,22 +9,21 @@ $this->title = '欢迎使用';
     <div class="jumbotron">
         <h1>欢迎使用!</h1>
 
-        <p class="lead">欢迎使用YiiBoot通用后台</p>
+        <p class="lead">欢迎使用Yii通用后台</p>
 
-        <p><a class="btn btn-lg btn-success" target="_blank" href="http://git.oschina.net/penngo/chadmin">开源地址</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                
+
             </div>
             <div class="col-lg-4">
-                
+
             </div>
             <div class="col-lg-4">
-               
+
             </div>
         </div>
 
